dfcf7c68bc22b52fcd8f61bf61d53cc187d89b90
smannan <smannan95@hotmail.com>
2019-11-20 22:06:23 -0800

    update endpoint to include python packages

--- machine-learning/serverless-machine-learning/api/app.py
+++ machine-learning/serverless-machine-learning/api/app.py
@@ -1,6 +1,7 @@
 from flask import Flask, request, json
 import boto3
 import pickle
+import random
 
 BUCKET_NAME = 'gosat-models'
 MODEL_FILE_NAME = 'gosat_random_forest'
@@ -10,31 +11,22 @@ S3 = boto3.client('s3', region_name='eu-central-1')
 
 @app.route('/', methods=['POST'])
 
-def load_model(key):    
-    # Load model from S3 bucket
-    response = S3.get_object(Bucket=BUCKET_NAME, Key=key)
-
-    # Load pickle model
-    model_str = response['Body'].read()     
-    model = pickle.loads(model_str)     
-    
-    return model
-
 def index():    
     # Parse request body for model input 
     body_dict = request.get_json(silent=True)    
     data = body_dict['data']     
     
-    # Load model
-    model = load_model(MODEL_FILE_NAME)
+    # Load model from S3 bucket
+    response = S3.get_object(Bucket=BUCKET_NAME, Key=MODEL_FILE_NAME)
+
+    # Load pickle model
+    model_str = response['Body'].read()     
+    model = pickle.loads(model_str)     
 
     # Make prediction 
-    prediction = model.predict(data).tolist()
-
+    # prediction = model.predict(data).tolist()
     # Respond with prediction result
-    result = {'prediction': prediction}
-
-    print (json.dumps(result))
+    # result = {'prediction': prediction}
 
     result = {
         'statusCode': 200,
@@ -48,10 +40,10 @@ def index():
             'sat': 0.39012123,
             'act': 0.11294921129
         }
-    } 
+    }
    
-    return json.dump(result)
+    return json.dumps(result)
 
-if __name__ == '__main__':  
+if __name__ == '__main__':    
     # listen on all IPs 
     app.run(host='0.0.0.0')
