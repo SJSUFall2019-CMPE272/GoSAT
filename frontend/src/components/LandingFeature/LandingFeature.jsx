20d22101bf6487a29a5524b5cfcc6029d2a7f41e
Kunj Parikh <parikhkunj@gmail.com>
2019-11-26 13:35:20 -0800

    Adding border and Aligning content to the center.

--- frontend/src/components/LandingFeature/LandingFeature.jsx
+++ frontend/src/components/LandingFeature/LandingFeature.jsx
@@ -22,14 +22,14 @@ const LandingFeature = (props) => (
       <h1 className="text-center">Step to Success</h1>
       <Row className="row-grid justify-content-center align-items-center">
         <Col className = "px-2 py-2" lg="3">
-          <Card className="card-stats">
+          <Card className="card-coin card-plain text-center">
             <CardBody>
               <div className="info">
                 <div className="icon icon-danger">
                   <i className="fas fa-medal" />
                 </div>
-                <h4 className="info-title">Mention your Achievements</h4>
-                <hr className="line-danger align-item-center" />
+                <h4 className="title info-title">Mention your Achievements</h4>
+                <hr className="line-danger" />
                 <p>
                   Mention all your Achievements like Academic scroes,SAT, ACT, Extracurriculars
                 </p>
@@ -38,13 +38,13 @@ const LandingFeature = (props) => (
           </Card>
         </Col>
         <Col className = "px-2 py-2" lg="3">
-          <Card className="card-stats">
+          <Card className="card-coin card-plain text-center">
             <CardBody>
               <div className="info">
                 <div className="icon icon-danger">
                   <i className="fas fa-chart-line" />
                 </div>
-                <h4 className="info-title">We provide insights</h4>
+                <h4 className="title info-title">We provide insights</h4>
                 <hr className="line-danger" />
                 <p>
                   We will provide all the insights. Things you lack at, things you are good.
@@ -53,14 +53,14 @@ const LandingFeature = (props) => (
             </CardBody>
           </Card>
         </Col>
-        <Col className = "px-2 py-2" lg="3">
-          <Card className="card-stats">
+        <Col className = "px-2 py-2" lg="3 ">
+          <Card className="card-coin card-plain text-center">
             <CardBody>
               <div className="info">
                 <div className="icon icon-danger">
                   <i className="fas fa-graduation-cap" />
                 </div>
-                <h4 className="info-title">Your Dream graduation</h4>
+                <h4 className="title info-title">Your dream graduation</h4>
                 <hr className="line-danger" />
                 <p>
                   By following the actionable guide and useful insights, you graduate from your dream university.
