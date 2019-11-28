d9e87788f610b2caecd002d6258be0b27fd9a817
smannan <smannan95@hotmail.com>
2019-11-28 06:59:16 -0800

    add build directory

---
+++ frontend/build/precache-manifest.1bb61d3279d235e39d2b67132cd522b4.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "797dec3b4a4988b6e33ce0f50deecc06",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2a4399c2e1f38ff7b1db",
+    "url": "/static/css/2.b0bb7f2c.chunk.css"
+  },
+  {
+    "revision": "b93af9db47c13a8c9c08",
+    "url": "/static/css/main.bf0e4867.chunk.css"
+  },
+  {
+    "revision": "2a4399c2e1f38ff7b1db",
+    "url": "/static/js/2.fbb0cac6.chunk.js"
+  },
+  {
+    "revision": "b93af9db47c13a8c9c08",
+    "url": "/static/js/main.f037d20a.chunk.js"
+  },
+  {
+    "revision": "9aabb23bcbd2c9e69d36",
+    "url": "/static/js/runtime-main.baeb1881.js"
+  },
+  {
+    "revision": "99c7dbef90707d2680c4ab1e9dadca67",
+    "url": "/static/media/boy-master.99c7dbef.png"
+  },
+  {
+    "revision": "b81dabc0bc6ca52fce921e3a928d5e0f",
+    "url": "/static/media/dots.b81dabc0.png"
+  },
+  {
+    "revision": "08a1679d811600c6b6443b925bf5d2cc",
+    "url": "/static/media/explorer.08a1679d.png"
+  },
+  {
+    "revision": "a27c6aed44630a169d87106b4ad8be2d",
+    "url": "/static/media/glass-boy.a27c6aed.png"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/static/media/logo.25bf045c.svg"
+  },
+  {
+    "revision": "07b6c8e5dd973fb05b3b8323a34ce737",
+    "url": "/static/media/old-master.07b6c8e5.png"
+  },
+  {
+    "revision": "87044275ba4259d6138f9a3e5d5c4acc",
+    "url": "/static/media/wizard-boy.87044275.png"
+  }
+]);
