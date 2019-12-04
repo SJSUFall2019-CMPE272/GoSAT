0878d4e7ddfa5237e08905b95fc30b517834e439
smannan <smannan95@hotmail.com>
2019-12-03 21:40:10 -0800

    update training code

--- machine-learning/train.py
+++ machine-learning/train.py
@@ -1,14 +1,20 @@
-import boto3
-import logging
+
+# coding: utf-8
+
+# In[ ]:
+
 import numpy as np
 import pandas as pd
 import pickle
 
-from botocore.exceptions import ClientError
-from sklearn.datasets import make_classification
-from sklearn.ensemble import RandomForestRegressor
+from sklearn.linear_model import LogisticRegression
+from sklearn import metrics
+from sklearn.model_selection import train_test_split
 from sklearn import preprocessing
-from StringIO import StringIO
+from sklearn.metrics import plot_confusion_matrix
+
+
+# In[ ]:
 
 """
 Transform input data for model
@@ -18,12 +24,13 @@ Return the one-hot encoder and combined numpy array
 """
 def transform_input(df):
     df.applymap(lambda s: s.lower() if type(s) == str else s)
+    df = df.drop(['Campus'], axis=1)
 
     encoder = preprocessing.OneHotEncoder(handle_unknown='ignore')
-    categorical_cols = ['Ethnicity', 'Gender', 'County', 'Campus']
+    categorical_cols = ['Ethnicity', 'Gender', 'County']
     categorical = df[categorical_cols].values
 
-    non_categorical_cols = ['GPA', 'ACT', 'AvgScrRead', 'AvgScrMath', 'AvgScrWrit']
+    non_categorical_cols = ['GPA', 'AvgScrRead', 'AvgScrMath', 'AvgScrWrit']
     intermediate = df[non_categorical_cols]
 
     categoricl_transformed = encoder.fit_transform(categorical).toarray()
@@ -31,32 +38,69 @@ def transform_input(df):
 
     return encoder, features
 
-def main():
-    # Where to pull the data from
-    # could be an S3 bucket, csv, etc...
-    test_bucket_name = 'gosat-data'
-    test_object_name = 'sample-data.csv'
-    S3 = boto3.client('s3', region_name='eu-central-1')
 
-    # Read the data from S3
-    response = S3.get_object(Bucket=test_bucket_name, Key=test_object_name)
-    data = response['Body'].read().decode('utf-8')
-    df = pd.read_csv(StringIO(data))
+# In[ ]:
 
-    # transform the data for the model
-    # return the encoder as it is used in predictions
-    encoder, X = transform_input(df)
-    y = df['Probability']
+df = pd.read_csv('../data/applicant_admissions_data.csv').drop(['Unnamed: 0', 'Status'], axis=1)
+df.head()
 
-    # train model
-    clf = RandomForestRegressor(n_estimators=100, max_depth=2, random_state=0)
-    clf.fit(X, y)
 
-    # upload mode and encoder to AWS for prediction
-    pickle.dump(clf, open('models/gosat_random_forest', 'w'))
-    pickle.dump(encoder, open('models/gosat_random_forest_encoder', 'w'))
-    S3.upload_file('models/gosat_random_forest', 'gosat-models', 'gosat_random_forest')
-    S3.upload_file('models/gosat_random_forest_encoder', 'gosat-models', 'gosat_random_forest_encoder')
+# In[ ]:
 
-if __name__ == '__main__':
-    main()
+# transform the data for the model
+# return the encoder as it is used in predictions
+encoder, X = transform_input(df)
+
+print ("X dimensions {0}".format(X.shape))
+
+le = preprocessing.LabelEncoder()
+y = le.fit_transform(df['Campus'])
+
+print ("y dimensions {0}".format(y.shape))
+
+# split data into 80% train 20% test
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+
+print ("X train dimensions {0}".format(X_train.shape))
+print ("y train dimensions {0}".format(y_train.shape))
+
+# train model
+clf = LogisticRegression(
+    random_state=0,
+    solver='lbfgs',
+    multi_class='multinomial',
+    max_iter=500)
+
+clf.fit(X_train, y_train)
+
+# upload mode and encoder to AWS for prediction
+pickle.dump(clf, open('models/gosat_logistic_regression', 'w'))
+pickle.dump(encoder, open('models/gosat_logistic_encoder', 'w'))
+
+
+# In[ ]:
+
+# train model
+clf = LogisticRegression(random_state=0, solver='lbfgs', multi_class='multinomial')
+clf.fit(X_train, y_train)
+
+y_pred = clf.predict(X_test)
+print ("predicted dimensions {0}".format(y_pred.shape))
+print ("y test dimensions {0}".format(y_test.shape))
+
+print (y_pred)
+print (y_test)
+
+disp = plot_confusion_matrix(clf, X_test, y_test,
+                                 display_labels=list(df['Campus'].unique()),
+                                 cmap=plt.cm.Blues,
+                                 normalize=true)
+
+print(disp.confusion_matrix)
+
+# upload mode and encoder to AWS for prediction
+#pickle.dump(clf, open('models/gosat_random_forest', 'w'))
+#pickle.dump(encoder, open('models/gosat_random_forest_encoder', 'w'))
+
+
+
